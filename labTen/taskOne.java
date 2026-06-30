079a37c8b6e16912fa2dec8127b6d000860992b0
Sifat Nizami <sifatnizami18@gmail.com>
2026-07-01 00:00:59 +0600

    Create taskOne.java

---
+++ labTen/taskOne.java
@@ -0,0 +1,26 @@
+class Student {
+    private String name;
+    private int id;
+    private double cgpa;
+
+    public void setName(String name) { this.name = name; }
+    public void setId(int id) { this.id = id; }
+    public void setCgpa(double cgpa) { this.cgpa = cgpa; }
+
+    public String getName() { return name; }
+    public int getId() { return id; }
+    public double getCgpa() { return cgpa; }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.setName("Sifat Nizami");
+        s.setId(3050);
+        s.setCgpa(3.00);
+
+        System.out.println(s.getName());
+        System.out.println(s.getId());
+        System.out.println(s.getCgpa());
+    }
+}
