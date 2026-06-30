957623a2d3c8239fedcf72bd524b58f911fc08ea
Sifat Nizami <sifatnizami18@gmail.com>
2026-07-01 00:04:41 +0600

    Update taskOne.java

--- labNine/taskOne.java
+++ labNine/taskOne.java
@@ -1,30 +1,26 @@
-class Animal {
-void sound() {
-System.out.println("Animal: Sound"); 
-}
+class Student {
+    private String name;
+    private int id;
+    private double cgpa;
+
+    public void setName(String name) { this.name = name; }
+    public void setId(int id) { this.id = id; }
+    public void setCgpa(double cgpa) { this.cgpa = cgpa; }
+
+    public String getName() { return name; }
+    public int getId() { return id; }
+    public double getCgpa() { return cgpa; }
 }
 
-class Cat extends Animal {
-@Override
-void sound() {
-System.out.println("Cat: Meow, Meow"); 
-}
-}
+public class Main {
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.setName("Sifat Nezami");
+        s.setId(253050);
+        s.setCgpa(3.00);
 
-class Dog extends Animal {
-@Override
-void sound() {
-System.out.println("Dog: Bark, Bark");  
-}
-}
-
-public class Main5 {
-public static void main(String[] args) {
-Animal animal = new Animal(); 
-Cat cat = new Cat();         
-Dog dog = new Dog();           
-animal.sound();  
-cat.sound();    
-dog.sound();   
-}
+        System.out.println(s.getName());
+        System.out.println(s.getId());
+        System.out.println(s.getCgpa());
+    }
 }
