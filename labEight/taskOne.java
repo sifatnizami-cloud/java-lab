5d2de4db12efc8270bea2713005a71f2004251f3
Sifat Nizami <sifatnizami18@gmail.com>
2026-06-30 23:51:57 +0600

    Create taskOne.java

---
+++ labEight/taskOne.java
@@ -0,0 +1,30 @@
+class Animal {
+void sound() {
+System.out.println("Animal: Sound"); 
+}
+}
+
+class Cat extends Animal {
+@Override
+void sound() {
+System.out.println("Cat: Meow, Meow"); 
+}
+}
+
+class Dog extends Animal {
+@Override
+void sound() {
+System.out.println("Dog: Bark, Bark");  
+}
+}
+
+public class Main5 {
+public static void main(String[] args) {
+Animal animal = new Animal(); 
+Cat cat = new Cat();          
+Dog dog = new Dog();         
+animal.sound(); 
+cat.sound();  
+dog.sound();    
+}
+}
